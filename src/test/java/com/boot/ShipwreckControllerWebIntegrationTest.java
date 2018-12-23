c9b51f1135f06b2576dd32153b28bf572e2d3793
Chen Tan <admin8978boy@gmail.com>
2018-12-23 16:12:00 +0800

    feat: adding more repo and models

--- src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
+++ src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,13 +17,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(App.class)
-@WebIntegrationTest
+//@SpringApplicationConfiguration(App.class)
+@SpringBootTest(classes = App.class)
+//@WebIntegrationTest
 public class ShipwreckControllerWebIntegrationTest {
 	@Test
 	public void testListAll() throws IOException {
-		RestTemplate restTemplate = new TestRestTemplate();
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8088/api/v1/shipwrecks", String.class);
+//		RestTemplate restTemplate = new TestRestTemplate();
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:8088/api/v1/shipwrecks", String.class);
 
 	    assertThat( response.getStatusCode() , equalTo(HttpStatus.OK));
 
--- src/test/java/com/boot/ShipwreckRepositoryIntegrationTest.java
+++ src/test/java/com/boot/ShipwreckRepositoryIntegrationTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.boot.model.Shipwreck;
@@ -15,7 +15,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(App.class)
+//@SpringApplicationConfiguration(App.class)
+@SpringBootTest(classes = App.class)
+
 public class ShipwreckRepositoryIntegrationTest {
 
 	@Autowired
