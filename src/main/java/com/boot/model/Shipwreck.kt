85ad22ee103f3ff09586dd4bb896aa6cd5256f4e
Chen Tan <admin8978boy@gmail.com>
2018-12-23 13:27:38 +0800

    feat: using an kotlin file

---
+++ src/main/java/com/boot/model/Shipwreck.kt
@@ -0,0 +1,42 @@
+package com.boot.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Shipwreck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+    var name: String = ""
+    var description: String = ""
+    var condition: String = ""
+    var depth: Int? = null
+    var latitude: Double? = null
+    var longitude: Double? = null
+    var yearDiscovered: Int? = null
+
+    constructor() {}
+
+    constructor(
+        id: Long?,
+        name: String,
+        description: String,
+        condition: String,
+        depth: Int?,
+        latitude: Double?,
+        longitude: Double?,
+        yearDiscovered: Int?
+    ) {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.condition = condition
+        this.depth = depth
+        this.latitude = latitude
+        this.longitude = longitude
+        this.yearDiscovered = yearDiscovered
+    }
+}
