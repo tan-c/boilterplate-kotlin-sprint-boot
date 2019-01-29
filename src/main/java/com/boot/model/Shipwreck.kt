6e3dfa4d8c1af3975f4327c57f3027b2a0e57895
Chen Tan <admin8978boy@gmail.com>
2019-01-29 18:56:21 +0800

    feat: adidng kotlin reflect

--- src/main/java/com/boot/model/Shipwreck.kt
+++ src/main/java/com/boot/model/Shipwreck.kt
@@ -4,9 +4,6 @@ import com.boot.service.BaseRepository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 
 @Entity
 data class Shipwreck (
--- src/main/java/com/boot/service/BaseContext.kt
+++ src/main/java/com/boot/service/BaseContext.kt
@@ -3,16 +3,10 @@ package com.boot.service
 import com.boot.model.BaseModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
-import java.util.Date
 import javax.persistence.EntityManager
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.MappedSuperclass
 import javax.persistence.PersistenceContext
 
-interface BaseRepository<T : BaseModel> : CrudRepository<T, Long>, JpaRepository<T, Long>
+interface BaseRepository<T : BaseModel> : JpaRepository<T, Long>
 
 open class BaseContext<R : BaseRepository<*>> {
     @Autowired
@@ -20,5 +14,4 @@ open class BaseContext<R : BaseRepository<*>> {
 
     @PersistenceContext
     protected lateinit var em: EntityManager
-
 }
