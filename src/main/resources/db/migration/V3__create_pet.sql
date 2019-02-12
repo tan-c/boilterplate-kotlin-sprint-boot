def3aa54095aaa279128eba6d5872f6882104660
Chen Tan <admin8978boy@gmail.com>
2019-02-12 18:34:45 +0800

    fix: using lower case db

--- src/main/resources/db/migration/V3__create_pet.sql
+++ src/main/resources/db/migration/V3__create_pet.sql
@@ -1,4 +1,6 @@
-CREATE TABLE pet(
+-- CREATE SCHEMA "pet" # This will be able to create schema with 'pet' in lower case
+
+CREATE TABLE "pet"(
 	id INT AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	gender VARCHAR(100) NOT NULL,
@@ -7,16 +9,16 @@ CREATE TABLE pet(
   owner_id INT
 );
 
-CREATE TABLE owner(
+CREATE TABLE "owner"(
 	ID INT AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	gender VARCHAR(255) NOT NULL,
   email VARCHAR(255)
 );
 
-INSERT INTO owner (name, gender) values ('Owner 1', 'male');
-INSERT INTO owner (name, gender) values ('Owner 2', 'female');
+INSERT INTO "owner" (name, gender) values ('Owner 1', 'male');
+INSERT INTO "owner" (name, gender) values ('Owner 2', 'female');
 
-INSERT INTO pet (name, gender, owner_id) values ('Wang Cai', 'male', 1);
-INSERT INTO pet (name, gender, owner_id) values ('Mantou', 'female', 1);
-INSERT INTO pet (name, gender, owner_id) values ('Huacai', 'female', 2);
+INSERT INTO "pet" (name, gender, owner_id) values ('Wang Cai', 'male', 1);
+INSERT INTO "pet" (name, gender, owner_id) values ('Mantou', 'female', 1);
+INSERT INTO "pet" (name, gender, owner_id) values ('Huacai', 'female', 2);
