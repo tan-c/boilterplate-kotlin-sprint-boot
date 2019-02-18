c9888123a31296da30bc5d088ee5022193bbfa5d
Chen Tan <admin8978boy@gmail.com>
2019-02-18 23:40:34 +0800

    feat: enable security config

--- src/main/java/com/boot/config/SecurityConfig.kt
+++ src/main/java/com/boot/config/SecurityConfig.kt
@@ -1,20 +1,35 @@
-//import org.springframework.context.annotation.Configuration
-////import org.springframework.security.config.annotation.web.builders.HttpSecurity
-////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-////
-////@Configuration
-////internal open class SecurityConfig : WebSecurityConfigurerAdapter() {
-////    @Throws(Exception::class)
-////    override fun configure(http: HttpSecurity) {
-////        http.authorizeRequests()
-////            .antMatchers(*AUTH_WHITELIST).permitAll()
-////            .antMatchers("/**/*").denyAll()
-////    }
-////
-////    companion object {
-////        private val AUTH_WHITELIST = arrayOf(
-////            // -- swagger ui
-////            "/swagger-resources/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**"
-////        )
-////    }
-////}
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+
+
+
+@Configuration
+internal open class SecurityConfig : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(web: WebSecurity?) {
+        web!!.ignoring().antMatchers(
+            "/v2/api-docs",
+            "/configuration/ui",
+            "/swagger-resources/**",
+            "/configuration/**",
+            "/swagger-ui.html",
+            "/webjars/**"
+        )
+    }
+
+//    @Throws(Exception::class)
+//    override fun configure(http: HttpSecurity) {
+//        http.authorizeRequests()
+//            .antMatchers(*AUTH_WHITELIST).permitAll()
+//            .antMatchers("/**/*").denyAll()
+//    }
+//
+//    companion object {
+//        private val AUTH_WHITELIST = arrayOf(
+//            // -- swagger ui
+//            "/swagger-resources/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**"
+//        )
+//    }
+}
